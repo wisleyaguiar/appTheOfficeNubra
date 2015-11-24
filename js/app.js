9e0a4dfeb6497fd9122fdc4245b668caab30f138
wisleyaguiar <wisleyaguiar@hotmail.com>
2015-11-24 08:50:40 -0200

    Ultima versão feita

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wisleyaguiar on 24/11/15.
+ */
+
+$("#abertura").show();
+
+$("#cadastro").hide();
+
+$('#home').hide();
+
+$('#bucarLojas').hide();
+
+$('#infoLojas').hide();
+
+$('#mapaLocal').hide();
+
+$('#newsPage').hide();
+
+$('#eventosPage').hide();
