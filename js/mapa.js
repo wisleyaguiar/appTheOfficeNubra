77866aab3577a58cd4091a6dfabd0186c3681d15
wisleyaguiar <wisleyaguiar@hotmail.com>
2015-11-28 00:44:47 -0200

    Ultima versão feita 90

--- js/mapa.js
+++ js/mapa.js
@@ -8,25 +8,6 @@ var endPartida;
 
 function initialize() {
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-
-            /*pontoPadrao = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-             map.setCenter(pontoPadrao);*/
-
-            var geocoder = new google.maps.Geocoder();
-
-            geocoder.geocode({
-                    "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-                },
-                function(results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        endPartida = results[0].formatted_address;
-                    }
-                });
-        });
-    }
-
     directionsDisplay = new google.maps.DirectionsRenderer();
     var latlng = new google.maps.LatLng(-23.5535238, -46.6539797);
 
@@ -55,6 +36,23 @@ initialize();
 
 $("#btTracarRota").click(function(event) {
     event.preventDefault();
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        /*pontoPadrao = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(pontoPadrao);*/
+
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({
+                "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+            },
+            function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    endPartida = results[0].formatted_address;
+                }
+            });
+    });
 
     var enderecoChegada = "R. Frei Caneca, 558 - Consolação, São Paulo - SP, 01307-001";
 
